3ac57bab3fb5aa2ce6ab26ac9b01cbea9f49be2f
denistopic-art <denis.topic@2gimnazija.edu.ba>
2026-04-09 21:34:31 +0200

    Dodano citanje podataka iz JSON baze

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
 
 app.use(express.json());
 
+const DATA_FILE = path.join(__dirname, "data.json");
+
+function readData() {
+  const data = fs.readFileSync(DATA_FILE, "utf8");
+  return JSON.parse(data);
+}
+
 app.get("/", (req, res) => {
   res.send("Server radi!");
+});
+
+app.get("/projekcije", (req, res) => {
+  const data = readData();
+  res.json(data);
 });
 
 app.listen(3000, () => {
